730e6fa8a16f8768b865907946618afa8b63a080
Dcarsanb <david@caraballo.me>
2026-04-15 08:26:31 +0200

    commit david

---
+++ VideoGroupProject/src/main/java/TrabajoColaborativoUD8/src/main/java/com/mycompany/trabajocolaborativoud8/ProductDataAccessException.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TrabajoColaborativoUD8.src.main.java.com.mycompany.trabajocolaborativoud8;
+
+/**
+ *
+ * @author Usuario25
+ */
+public class ProductDataAccessException extends Exception {
+
+    public ProductDataAccessException() {
+    }
+
+    public ProductDataAccessException(String message) {
+        super(message);
+    }
+
+    
+    
+}
